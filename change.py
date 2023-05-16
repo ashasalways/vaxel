6e2ff35d014fa15054bf3e0409ccb26c93ec5b63
Filip Eddebring <fied1225@storsjogymnasiet.se>
2023-05-16 10:45:24 +0100

    klar

--- change.py
+++ change.py
@@ -4,3 +4,54 @@ def exChange(belopp, sedel):
 def get_exchange_dict(inbetalning, priset):
     antal_mynt = 0
     pengar_tillbaka = 0
+
+    sedlar_mynt_dictionary = {500:0, 200:0, 100:0, 50:0, 20:0, 10:0, 5:0, 2:0, 1:0}
+
+    #500-----------------------------------------
+    pengar_tillbaka = inbetalning - priset
+    antal_mynt = exChange(pengar_tillbaka, 500)
+    pengar_tillbaka = pengar_tillbaka % 500
+    sedlar_mynt_dictionary[500]
+
+    #200-----------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 200)
+    pengar_tillbaka = pengar_tillbaka % 200
+    sedlar_mynt_dictionary[200] = antal_mynt
+
+    #100-----------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 100)
+    pengar_tillbaka = pengar_tillbaka % 100
+    sedlar_mynt_dictionary[100] = antal_mynt
+
+    #50------------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 50)
+    pengar_tillbaka = pengar_tillbaka % 50
+    sedlar_mynt_dictionary[50] = antal_mynt
+
+    #20------------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 20)
+    pengar_tillbaka = pengar_tillbaka % 20
+    sedlar_mynt_dictionary[20] = antal_mynt
+
+    #10------------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 10)
+    pengar_tillbaka = pengar_tillbaka % 10
+    sedlar_mynt_dictionary[10] = antal_mynt
+
+    #5-------------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 5)
+    pengar_tillbaka = pengar_tillbaka % 5
+    sedlar_mynt_dictionary[5] = antal_mynt
+
+    #2-------------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 2)
+    pengar_tillbaka = pengar_tillbaka % 2
+    sedlar_mynt_dictionary[2] = antal_mynt
+
+    #1-------------------------------------------
+    antal_mynt = exChange(pengar_tillbaka, 1)
+    pengar_tillbaka = pengar_tillbaka % 1
+    sedlar_mynt_dictionary[1] = antal_mynt
+
+    #print (f"500: {antal_mynt}")
+    return sedlar_mynt_dictionary
--- start.py
+++ start.py
@@ -6,14 +6,30 @@ def main():
     print("\n\t-Växlare-\n")
 
     pris = input("\tMata in pris på varan i kr: ")
-    inbet = input("\tInbetalt belopp i kr:")
+    inbet = input("\tInbetalt belopp i kr: ")
 
     exChangeNow(int(pris), int(inbet))
 
 def exChangeNow(priset, inbetalningen):
     
-    if priset > inbetalningen():
-        print("\tFattas pengar!")
+    if (priset > inbetalningen):
+        print("\tFattas pengar! ")
+
+    else:
+        change_back = change.get_exchange_dict(inbetalningen, priset)
+
+        print("\n\t-------------------------------------------")
+        print ("\tVäxel tillbaka: \n")
+        print(f"\tAntal 500 lappar: {change_back[500]}")
+        print(f"\tAntal 200 lappar: {change_back[200]}")
+        print(f"\tAntal 100 lappar: {change_back[100]}")
+        print(f"\tAntal 50 lappar: {change_back[50]}")
+        print(f"\tAntal 20 lappar: {change_back[20]}")
+        print(f"\tAntal 10 kronor: {change_back[10]}")
+        print(f"\tAntal 5 kronor: {change_back[5]}")
+        print(f"\tAntal 2 kronor: {change_back[2]}")
+        print(f"\tAntal 1 kronor: {change_back[1]}")
+
 
 
 
